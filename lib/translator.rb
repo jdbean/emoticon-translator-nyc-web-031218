de0d22b80a219d211fc843aad1b7cf4496b8e13e
J.D. Bean <jonathandbean@gmail.com>
2018-02-25 17:17:11 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,23 @@
 require "yaml"
 require "pry"
 
+# #load_library
+#   accepts one argument, the file path
+#   return value
+#     returns a hash
+#     has two keys, 'get_meaning' and 'get_emoticon' 
+#     the keys 'get_meaning' and 'get_emoticon' point to inner hashes 
+#     the keys inside the 'get_meaning' hash are the Japanese emoticons 
+#     the emoticon keys inside the 'get_meaning' hash point to their meanings
+#     the keys inside the 'get_emoticon' hash are the English emoticons
+#     the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents
 
 def load_library(path)
   emoticons = YAML.load_file(path)
+  binding.pry
+  emoticons.each_with_object |(meaning, emoticon_array), new_hash
+  new_hash[get_meaning] = {}
+  new_hash[get_emoticon] = {}
   binding.pry
 end
 
